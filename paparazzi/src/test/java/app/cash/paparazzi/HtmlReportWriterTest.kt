c3963d393400650ca8ade829ce66c0d3fb4cc68b
Brian Gardner <bgardner@squareup.com>
2024-08-12 12:07:24 -0400

    Add test to verify the hash value of three similar images are different

--- paparazzi/src/test/java/app/cash/paparazzi/HtmlReportWriterTest.kt
+++ paparazzi/src/test/java/app/cash/paparazzi/HtmlReportWriterTest.kt
@@ -27,6 +27,7 @@ import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.attribute.FileTime
 import java.time.Instant
 import java.util.Date
+import javax.imageio.ImageIO
 
 class HtmlReportWriterTest {
   @get:Rule
@@ -266,9 +267,53 @@ class HtmlReportWriterTest {
     }
   }
 
+  // Verify that views of different size but the same color result in different hash files
+  @Test
+  fun similarImagesProduceDifferentHashes() {
+    val horizontal = ImageIO.read(File("src/test/resources/horizontal.png"))
+    val convertedHorizontal = horizontal.convertImage()
+    val vertical = ImageIO.read(File("src/test/resources/vertical.png"))
+    val convertedVertical = vertical.convertImage()
+    val square = ImageIO.read(File("src/test/resources/square.png"))
+    val convertedSquare = square.convertImage()
+
+    // Feed the images to HtmlReportWriter
+    val htmlReportWriter = HtmlReportWriter("run_one", reportRoot.root)
+    htmlReportWriter.use {
+      val frameHandler = htmlReportWriter.newFrameHandler(
+        Snapshot(
+          name = "loading",
+          testName = TestName("app.cash.paparazzi", "CelebrityTest", "testSettings"),
+          timestamp = Instant.parse("2019-03-20T10:27:43Z").toDate(),
+          tags = listOf("redesign")
+        ),
+        frameCount = 1,
+        fps = -1
+      )
+      frameHandler.use {
+        frameHandler.handle(convertedHorizontal)
+        frameHandler.handle(convertedVertical)
+        frameHandler.handle(convertedSquare)
+      }
+    }
+    // Verify that there are 3 files in the reports/paparazzi/images file
+    // This should confirm that the snapshot images used in the test report are correct
+    assertThat(File(reportRoot.root, "images").listFiles()!!.size).isEqualTo(3)
+  }
+
   private fun Instant.toDate() = Date(toEpochMilli())
 
   private fun File.lastModifiedTime(): FileTime {
     return Files.readAttributes(this.toPath(), BasicFileAttributes::class.java).lastModifiedTime()
   }
+
+  // Function that takes in an existing Buffered image and converts its type to one
+  // that ApngWriter can consume
+  private fun BufferedImage.convertImage() = BufferedImage(
+      width,
+      height,
+      BufferedImage.TYPE_INT_ARGB_PRE // Something ApngWriter can process
+    ).apply {
+      graphics.drawImage(this, 0, 0, null)
+    }
 }
